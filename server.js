b75c8a62a32ffe8be75be9eb1771bf8f008f72bd
melvinroest <melvinroest@gmail.com>
2017-02-15 22:48:45 +0100

    clients now spawn their own child process

--- server.js
+++ server.js
@@ -3,27 +3,36 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const spawn = require('child_process').spawn;
-const sh = spawn('bash');
 
 app.use(express.static('./'))
 
-sh.stdout.on('data', function(data) {
-  io.emit('message', data);
-});
-
-sh.stderr.on('data', function(data) {
-  io.emit('message', data);
-});
-
-sh.on('exit', function (code) {
-  io.emit('exit', '** Shell exited: '+code+' **');
-});
-
 io.on('connection', function(client){
+  const sh = spawn('bash');
+  console.log('a client connected! -- ' + client.id);
+  // console.log(client.id + ": " + client.handshake.headers['user-agent']);
+
+
+  sh.stdout.on('data', function(data) {
+    io.emit('message', data);
+  });
+
+  sh.stderr.on('data', function(data) {
+    io.emit('message', data);
+  });
+
+  sh.on('exit', function (code) {
+    io.emit('exit', '** Shell exited: '+code+' **');
+  });
+
   client.on('message', function(data){
     sh.stdin.write(data+"\n");
     io.emit('message', new Buffer("> "+data));
   });
+
+  client.on('disconnect', function(){
+    console.log('a client disconnected -- ' + client.id);
+    sh.kill('SIGINT');
+  })
 });
 
 server.listen(8080, function(){
